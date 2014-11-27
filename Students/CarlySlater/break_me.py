52c2b9543dbf5f439420f0c6135e01c3a64013f0
Carly Slater <carlyslater@gmail.com>
2014-11-26 18:20:38 -0800

    the whole enchilda, errors and all

--- Students/CarlySlater/break_me.py
+++ Students/CarlySlater/break_me.py
@@ -1,15 +1,16 @@
 #Problem 1 - Name Error
-#b = 1
-#c = 4
-#print(b+f)
+b = 1
+c = 4
+print(b+f)
 
 #Problem 2 - Type Error
-#c = 1
-#d = str(u"bobbby")
-#print(c-d)
+c = 1
+d = str(u"bobbby")
+print(c-d)
 
 #Problem 3 - Syntax Error
-#make me a sandwich
+make me a sandwich
+#sudo make me a sandwich?
 
 #Problem 4 - Attribute Error
 thingy = 4
