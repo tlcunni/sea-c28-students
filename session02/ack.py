91da41c13405103a76fb360358ba2c8b2e60d3cf
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-11-29 11:17:41 -0800

    draft 1 of ack, not working yet

---
+++ session02/ack.py
@@ -0,0 +1,11 @@
+#ack
+from __future__ import print_function
+
+def ack(m,n):
+	if m = 0:
+		return n+1
+	elif m>0 and n=0:
+		return (m-1,1)
+	else:
+		return (m-a, (m,n-1))
+#this is broken and doesn't work right now
