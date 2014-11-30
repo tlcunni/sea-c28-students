e61e806009bdb3cf38c6074a768d257ac67c44b0
Matthew Lee <ml7@uw.edu>
2014-11-29 20:23:24 -0800

    Completed Fibonacci series function for any positive value of n

--- Students/MatLee/series.py
+++ Students/MatLee/series.py
@@ -1,11 +1,21 @@
 def fibonacci(n):
     """Return the nth value of the Fibonacci series."""
     series = [0, 1]
-    
+	
     if n <= 2:
         return series[n-1]
     else:
-        pass
+        for x in range(n-2):
+            series[0], series[1] = series[1], series[0]
+            series[1] = series[0] + series[1]
+        return series[1]
+    """
+    #alternate method
+        while len(series) < n:
+            series.append(series[-2] + series[-1])
+        return series[-1]
+    """
 
-print fibonacci(1)
-print fibonacci(2)
+
+for x in range(1, 21):
+    print fibonacci(x),
