ffba7259833167cb958d307cbfb56822692d3e50
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-11-30 15:49:26 -0800

    test info for ack has been added, test has not been run

--- session02/ack.py
+++ session02/ack.py
@@ -9,8 +9,10 @@ def a(m,n):
 	if n == 0:
 		return a(m-1, 1)
 	return a(m-1, a(m, n-1))
-	#if __a(0,0)__ == __1__:
-	#	print "All tests passed"
+
+#if __name__ == __main__:
+#	assert a(0,0) == 1
+#	print "All tests passed"
 #assert a(1,0) == 2
 #assert a(2,0) == 3
 #assert a(3,0) == 5
@@ -21,7 +23,7 @@ def a(m,n):
 #assert a(1,1) == 3
 #assert a(2,1) == 5
 #assert a(3,1) == 13
-#assert a(1,2) == 4
+#assert a(1,2) == 4z
 #assert a(1,3) == 5
 #assert a(1,4) == 6
 #assert a(2,2) == 7
@@ -32,4 +34,4 @@ def a(m,n):
 #assert a(3,4) == 125
 
 
-print a(-1,1)
+print a(1,1)
