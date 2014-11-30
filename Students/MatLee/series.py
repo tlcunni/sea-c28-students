ec7d2c3a1c009174fde37cc5ea7e8a8ca246d231
Matthew Lee <ml7@uw.edu>
2014-11-29 17:23:00 -0800

    Creating series file, with the first case for the Fibonacci series (n is 1 or 2)

---
+++ Students/MatLee/series.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    """Return the nth value of the Fibonacci series."""
+    series = [0, 1]
+    
+    if n <= 2:
+        return series[n-1]
+    else:
+        pass
+
+print fibonacci(1)
+print fibonacci(2)
