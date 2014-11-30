9cd49b42c94438d46abb4535589f3e66396cb211
Matthew Lee <ml7@uw.edu>
2014-11-29 20:48:00 -0800

    Added sum_series function.

--- Students/MatLee/series.py
+++ Students/MatLee/series.py
@@ -16,9 +16,6 @@ def fibonacci(n):
         return series[-1]
     """
 
-#for x in range(1, 21):
-#    print fibonacci(x),
-
     
 def lucas(n):
     """Return the nth value of the Lucas number series."""
@@ -32,5 +29,22 @@ def lucas(n):
             series[1] = series[0] + series[1]
         return series[1]
 
-#for x in range(1, 21):
-#    print lucas(x),
+
+def sum_series(n, a=0, b=1):
+    """Return the nth value of a series of sums.
+    Generate a series n terms long by adding the most recent two terms.
+    Dictate the number of terms with parameter n, 
+    and first and second terms with 'a' and 'b' respectively.
+    'a' and 'b' are 0 and 1 by default."""
+    series = [a, b]
+	
+    if n <= 2:
+        return series[n-1]
+    else:
+        for x in range(n-2):
+            series[0], series[1] = series[1], series[0]
+            series[1] = series[0] + series[1]
+        return series[1]
+
+for x in range(1, 21):
+    print sum_series(x, 0, 1),
