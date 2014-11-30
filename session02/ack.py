c2015b3890855a6ae2dd8b093eb5383070d4bc16
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-11-30 15:01:20 -0800

    completed ackerman's function

--- session02/ack.py
+++ session02/ack.py
@@ -1,11 +1,11 @@
 #ack
-from __future__ import print_function
 
-def ack(m,n):
-	if m = 0:
-		return n+1
-	elif m>0 and n=0:
-		return (m-1,1)
-	else:
-		return (m-a, (m,n-1))
-#this is broken and doesn't work right now
+def a(m,n):
+	"""Return a value calculated with Ackerman's Function"""
+	if m == 0:
+		return n + 1
+	if n == 0:
+		return a(m-1, 1)
+	return a(m-1, a(m, n-1))
+
+print a(3,4)
