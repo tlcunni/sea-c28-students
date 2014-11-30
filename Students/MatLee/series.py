907307b175db35065fb89db72cdfe3f035cb88db
Matthew Lee <ml7@uw.edu>
2014-11-30 00:00:47 -0800

    Reduced redundancy, added a way to handle negative values of n, and added tests for the functions.

--- Students/MatLee/series.py
+++ Students/MatLee/series.py
@@ -1,16 +1,10 @@
 def fibonacci(n):
     """Return the nth value of the Fibonacci series."""
-    series = [0, 1]
-	
-    if n <= 2:
-        return series[n-1]
-    else:
-        for x in range(n-2):
-            series[0], series[1] = series[1], series[0]
-            series[1] = series[0] + series[1]
-        return series[1]
+    
+    return sum_series(n)
+    
     """
-    #alternate method
+        #alternate method
         while len(series) < n:
             series.append(series[-2] + series[-1])
         return series[-1]
@@ -19,15 +13,8 @@ def fibonacci(n):
     
 def lucas(n):
     """Return the nth value of the Lucas number series."""
-    series = [2, 1]
-	
-    if n <= 2:
-        return series[n-1]
-    else:
-        for x in range(n-2):
-            series[0], series[1] = series[1], series[0]
-            series[1] = series[0] + series[1]
-        return series[1]
+    
+    return sum_series(n, 2, 1)
 
 
 def sum_series(n, a=0, b=1):
@@ -36,8 +23,10 @@ def sum_series(n, a=0, b=1):
     Dictate the number of terms with parameter n, 
     and first and second terms with 'a' and 'b' respectively.
     'a' and 'b' are 0 and 1 by default."""
+    
     series = [a, b]
-	
+    if n < 1:
+        return None
     if n <= 2:
         return series[n-1]
     else:
@@ -46,5 +35,26 @@ def sum_series(n, a=0, b=1):
             series[1] = series[0] + series[1]
         return series[1]
 
-for x in range(1, 21):
-    print sum_series(x, 0, 1),
+        
+if __name__ == "__main__":
+    #the nth number of the fibonacci series should be generated
+    #by a call on sum_series(n)
+    #compare the first 10 numbers of the fibonacci series to the
+    #output of the function for n = 1 to 10
+    #if the function sum_series passes these tests, then the functions
+    #fibonacci and lucas will similarly perform as expected
+    fibonacci_check = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    for i in range(len(fibonacci_check)):
+        assert fibonacci_check[i] == sum_series(i+1)
+    
+    #the nth number of the lucas numbers should be generated
+    #by a call on sum_series(n, 2, 1)
+    #compare the first 10 numbers of the lucas numbers to the output
+    #of the function for n = 1 to 10
+    lucas_check = [2, 1, 3, 4, 7, 11, 18, 29, 47, 76]
+    for i in range(len(lucas_check)):
+        assert lucas_check[i] == sum_series(i+1, 2, 1)
+    
+    #check if the function returns None for negative values of n
+    assert sum_series(-1) == None
+    
