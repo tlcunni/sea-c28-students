fc53f16c3be01bf7e5bf39790f694a62dd7f082f
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-11-30 18:15:02 -0800

    tests working on series

--- session02/series.py
+++ session02/series.py
@@ -3,29 +3,33 @@ def fibonacci(n):
 	a , b = 0,1
 	for i in range(n):
 		a, b = b, a+b
-	print a
+	return a
 
-#fibonacci(9)
+#print fibonacci(9)
 
 def lucas(n):
 	"""Return "nth" value in the Lucas string"""
 	a , b = 2, 1
 	for i in range(n):
 		a, b = b, a+b
-	print a
+	return a
 
-#lucas(9)
+#print lucas(9)
 
 def sum_series(n,a=0,b=1):
 	"""Return "nth" value using formula similar to Fibonacci series, beginning with value "a" and "b" """
 	for i in range(n):
 		a, b = b, a+b
-	print a
+	return a
 
-#sum_series(9)
+#print sum_series(9)
+#print sum_series(9,2,1)
+
 
 
 if __name__ == '__main__':
-	assert fibonacci(9) == 34
-	assert lucas(9) == 76
-	assert sum_series(9) == 34
+    assert (fibonacci(9)) == 34
+    assert lucas(9) == 76
+    assert sum_series(9) == 34
+    assert sum_series(9,2,1) == 76
+    print "All Tests passed"
