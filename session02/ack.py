1e16814b052b3cbb7f2d93c8b71a476a887e789a
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-12-01 19:02:07 -0800

    Fix indentation on ackerman's function

--- session02/ack.py
+++ session02/ack.py
@@ -2,13 +2,13 @@
 
 def a(m,n):
 	"""Return a value calculated with Ackerman's Function, m,n are non-negative integers"""
-	if m<0 or n<0:
-		return "None"
-	if m == 0:
-		return n + 1
-	if n == 0:
-		return a(m-1, 1)
-	return a(m-1, a(m, n-1))
+    if m<0 or n<0:
+        return "None"
+    if m == 0:
+        return n + 1
+    if n == 0:
+        return a(m-1, 1)
+    return a(m-1, a(m, n-1))
 
 if __name__ == '__main__':
     assert a(0,0) == 1
