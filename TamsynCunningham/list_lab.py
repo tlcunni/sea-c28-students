0113bc50ce560066ef187268deb6bcde1e7f2829
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-12-02 20:14:10 -0800

    all fruit additions programmed

--- TamsynCunningham/list_lab.py
+++ TamsynCunningham/list_lab.py
@@ -3,8 +3,23 @@
 
 FRUIT = [u'Apples', u'Pears',u'Oranges',u'Peaches']
 
-print FRUIT 
+print FRUIT
 
-FRUIT.append((raw_input(u'Please add your favorite fruit -->  ')))
+FRUIT.append((raw_input(u'Please add your favorite fruit-> ')))
+
+print FRUIT
+
+user_input = input(u'Please select number from 1 to 5 ->  ')
+
+print user_input 
+print FRUIT[(user_input-1)]
+
+NEW_FRUIT = [u"Mango"]
+
+FRUIT = NEW_FRUIT + FRUIT
+
+print FRUIT
+
+FRUIT.insert(0,u"Bananna")
 
 print FRUIT
