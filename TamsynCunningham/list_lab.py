309dcaed470d35aab65d75cfbb0ce872ba4e6651
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-12-02 20:28:17 -0800

    pretty printing of fruit lists

--- TamsynCunningham/list_lab.py
+++ TamsynCunningham/list_lab.py
@@ -3,23 +3,36 @@
 
 FRUIT = [u'Apples', u'Pears',u'Oranges',u'Peaches']
 
-print FRUIT
+
+for _ in FRUIT:
+	print _
 
 FRUIT.append((raw_input(u'Please add your favorite fruit-> ')))
 
-print FRUIT
+for _ in FRUIT:
+	print _
 
 user_input = input(u'Please select number from 1 to 5 ->  ')
 
 print user_input 
 print FRUIT[(user_input-1)]
 
+print ""
+print ""
+
 NEW_FRUIT = [u"Mango"]
 
 FRUIT = NEW_FRUIT + FRUIT
 
-print FRUIT
+for _ in FRUIT:
+	print _
+
+print ""
+print ''
+
+
 
 FRUIT.insert(0,u"Bananna")
-
-print FRUIT
+	
+for _ in FRUIT:
+	print _
