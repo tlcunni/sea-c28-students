6a91f144d916f0474e67d6719f8cbe4808f62100
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-12-02 18:55:11 -0800

    list_lab created, initial list and first addition

---
+++ TamsynCunningham/list_lab.py
@@ -0,0 +1,11 @@
+#(#!/usr/bin/env python)
+#from__future__import print__function
+
+FRUIT = [u'Apples', u'Pears',u'Oranges',u'Peaches']
+
+print FRUIT 
+
+FRUIT.append(raw_input(u'Please add your favorite fruit -->  '))
+
+print FRUIT
+
