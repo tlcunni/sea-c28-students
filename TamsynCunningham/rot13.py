1dfce430e49fbae3288fceffde52b61647e10059
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-12-06 14:52:50 -0800

    ROT13 code works, no if__name__ tests yet

--- TamsynCunningham/rot13.py
+++ TamsynCunningham/rot13.py
@@ -2,8 +2,8 @@ import string
 
 rot13 = string.maketrans("ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz","NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
 
-def rottrans(code):
+def rottrans(x):
 	code = raw_input("What would you like to translate?  ")
-	print string.translate(raw_input(code,rot13)
+	print string.translate(code,rot13)
 
-print rottrans(bob)
+rottrans(1)
