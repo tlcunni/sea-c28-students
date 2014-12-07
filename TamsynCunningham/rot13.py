2e1be9ea420905c85db04bcaf162798a5a747788
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-12-07 11:54:06 -0800

    rot13 with tests

--- TamsynCunningham/rot13.py
+++ TamsynCunningham/rot13.py
@@ -2,10 +2,15 @@ import string
 
 rot13 = string.maketrans("ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz","NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
 
-def rottrans():
-	code = raw_input("What would you like to translate?  ")
-	print string.translate(code,rot13)
+def rottrans(code=None):
+	if code is None:
+	    code = raw_input("What would you like to translate?  ")
+	return string.translate(code,rot13)
 
-rottrans()
+print rottrans()
 
+print rottrans("Carly's Birthday")
 
+if __name__ == '__main__':
+	assert rottrans('the') == 'gur'
+	print "test passed"
